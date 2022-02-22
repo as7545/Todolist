a17aed1491ac59986ef0a9665549b5871146e845
Ashish Yadav <ay817985@gmail.com>
2022-02-22 17:45:31 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import TodoList from './Todolist';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>Todo App</h1>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
